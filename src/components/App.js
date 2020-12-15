43451ef24cc7647ce9b2375c54eb9e5555204586
rmartins85 <50893308+rmartins85@users.noreply.github.com>
2020-12-14 21:07:25 -0500

    updating

---
+++ src/components/App.js
@@ -0,0 +1,39 @@
+import React, {useState, useEffect} from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import Scroll from './Scroll';
+import '../styles/App.css';
+
+const App = () => {
+    const [robots, setRobots] = useState([]);
+    const [searchfield, setSearchfield] = useState("");
+
+	useEffect(() => {
+    	fetch('https://jsonplaceholder.typicode.com/users')
+    	.then(response => response.json())
+    	.then(users => setRobots(users))
+	}, []);
+
+	const onSearchChange = (event) => {
+		setSearchfield(event.target.value);
+	}
+
+	const filteredRobots = robots.filter(robot => {
+		return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+	})
+	
+	return (
+		robots.length === 0 ?
+			<h1>Loading</h1>
+    	:
+			<div className='tc'>
+				<h1 className='f1'>RoboFriends</h1>
+				<SearchBox searchChange={onSearchChange}/>
+				<Scroll>
+					<CardList robots={filteredRobots}/>
+				</Scroll>
+			</div>
+    );
+};
+
+export default App;
